e6e6d211af96f2d745811182918a02f88668bc23
JasonJacob07 <100529935+JasonJacob07@users.noreply.github.com>
2022-09-08 12:15:49 +0530

    thirdcommit

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ class Stack:
   def __init__(self):
     self.head = None
     
-''' If head is null, then we create new node and assign the head to the new node else if head is already present, create new node and connect the head to the new node.'''
+ #If head is null, then we create new node and assign the head to the new node else if head is already present, create new node and connect the head to the new node.
   def push(self, data) -> None:
     if self.head is None:
       new_Node = Node(data)
@@ -17,16 +17,14 @@ class Stack:
      new_Node.next = self.head
      self.head = new_Node
     
-'''if head is connected with some node, to remove the node we will change the head to the next node.'''
+#if head is connected with some node, to remove the node we will change the head to the next node.
   def pop(self) -> None:
      if self.head is not None:
       self.head = self.head.next
       
 
   def status(self):
-    """
-    It prints all the elements of stack.
-    """
+    #It prints all the elements of stack.
     temp = self.head
     while temp is not None:
       print (temp.data, end="=>")
